8908ca150c705db1d4bd9afe8ed1234833030bf3
kevin dhenin <dhenink@laposte.net>
2017-04-21 12:12:32 +0200

    Ajout d'option sur Sonata

--- src/AppBundle/Admin/PostAdmin.php
+++ src/AppBundle/Admin/PostAdmin.php
@@ -12,14 +12,24 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 class PostAdmin extends AbstractAdmin
 {
+    protected $datagridValues = [
+        'page' => 1,
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'id',
+    ];
+
     protected function configureFormFields(FormMapper $form)
     {
         $form
+            ->with('Content', ['class' => 'col-md-8'])
             ->add('title', TextType::class)
             ->add('slug', TextType::class)
             ->add('summary', TextType::class)
             ->add('content', TextareaType::class)
             ->add('publishedAt', DateTimePickerType::class)
+            ->end()
+
+            ->with('Meta data', ['class' => 'col-md-4'])
             ->add('author', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'fullName',
@@ -30,6 +40,7 @@ class PostAdmin extends AbstractAdmin
                 'multiple' => true,
                 'expanded' => false,
             ])
+            ->end()
         ;
     }
     protected function configureDatagridFilters(DatagridMapper $filter)
@@ -37,14 +48,35 @@ class PostAdmin extends AbstractAdmin
         $filter
             ->add('id')
             ->add('title')
+            ->add('tags', null, [], EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name',
+            ])
         ;
     }
     protected function configureListFields(ListMapper $list)
     {
         $list
             ->add('id')
-            ->add('title')
-            ->add('slug')
+            ->addIdentifier('title')
+            ->add('slug', null, ['editable' => true])
+            ->add('publishedAt')
+            ->add('_action', null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => ['template' => ':admin:delete_button.html.twig'],
+                ],
+            ])
         ;
     }
+
+    public function toString($object)
+    {
+        return $object instanceof Post
+            ? $object->getTitle()
+            : 'Blog Post';
+    }
+
+
 }
